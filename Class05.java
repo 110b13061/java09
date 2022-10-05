2e5ffc33de53a93af0cd3b8dc9a3e07de2c81098
110b13061 <113488308+110b13061@users.noreply.github.com>
2022-10-05 21:39:43 +0800

    Create Class05.java

---
+++ Class05.java
@@ -0,0 +1,16 @@
+package java09;
+
+class CCount{          //(a)
+	int cnt = 0;
+	public void count() {
+		cnt++;
+	}
+	public void setZero() {    //(b)
+		cnt = 0;
+	}
+	public void setValue(int n) {    //(c)
+		cnt = n;
+	}
+}
+//(d)類別變數
+//(e)類別函數
